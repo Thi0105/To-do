2114eecde733581f03d5631e75c437243299ec67
Thi0105 <dinhnhatthi0105@gmail.com>
2025-03-18 20:41:29 +0200

    Database connection and Routing

--- server/index.js
+++ server/index.js
@@ -1,50 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const { query } = require('./helpers/db.js')
+const { todoRouter } = require('./routes/todo.js')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extened: false}))
+app.use('/', todoRouter)
 
 const port = process.env.PORT
-
-app.get("/", async (req, res) => {
-    console.log(query)
-    try {
-        const result = await query('SELECT * FROM task')
-        const rows = result.rows? result.rows : []
-        res.status(200).json(rows)
-    } catch (error) {
-        console.log(error)
-        res.statusMessage = error
-        res.status(500).json({error: error})
-    }
-})
-
-
-app.post("/new", async (req, res) => {
-    try {
-        const result = await query('INSERT INTO task (description) VALUES ($1) RETURNING *',
-        [req.body.description])
-        res.status(200).json({id: result.rows[0].id})
-    } catch (error) {
-        console.log(error)
-        res.statusMessage = error
-        res.status(500).json({error: error})
-    }
-})
-
-app.delete("/delete/:id", async(req, res) => {
-    try {
-        const result = await query('DELETE FROM task WHERE id = $1', [id])
-        res.status(200).json({id: id})
-    } catch (error) {
-        console.log(error)
-        res.statusMessage = error
-        res.status(500).json({error: error})
-    }
-})
 
 app.listen(port)
